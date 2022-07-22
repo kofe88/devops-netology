ac4646c6cb981e4a0b3633100ec9781691a809bd
Nikolay Fedorov <kofe88@mail.ru>
2022-07-22 03:21:38 +0300

    [07-terraform-02-syntax] fix

--- virt-pdc-2/07-terraform-02-syntax/scripts/cloud-terraform/output.tf
+++ virt-pdc-2/07-terraform-02-syntax/scripts/cloud-terraform/output.tf
@@ -1,4 +1,4 @@
-utput "internal_ip_address_node01_yandex_cloud" {
+output "internal_ip_address_node01_yandex_cloud" {
   value = "${yandex_compute_instance.node01.network_interface.0.ip_address}"
 }
 
